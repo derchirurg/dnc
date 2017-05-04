21e31be38d982b2bfae9d455d8c79cd61aa49e00
derchirurg <bit-pm@gmx-topmail.de>
2017-05-04 12:20:19 +0200

    DI aus Konfigurationsdatei laden.

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +13,20 @@ namespace EmptyApp
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            // Konfigurationsdatei laden.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -19,10 +34,13 @@ namespace EmptyApp
             // Das wird für die DI benötigt.
             services.AddOptions();
 
-            // DI ReinRaus hinzufügen.
+            // Aus einer Konfigurationsdatei laden.
+            services.Configure<ReinRausOptions>(Configuration.GetSection("ReinRaus"));
+
+            // DI manuell hinzufügen.
             services.AddReinRaus((options) =>
             {
-                options.Number = "Eins";
+                // options.Number = "Fuenf";
             });
         }
 
